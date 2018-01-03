eb1980007950c3030756824cd181302849bef460
Mark R <mirubenstein@gmail.com>
2018-01-03 00:00:22 -0800

    Update based on solution from tenebrousedge and wisygig

--- etl.rb
+++ etl.rb
@@ -1,10 +1,8 @@
 class ETL
   def self.transform(old)
-    old.each_pair.with_object({}) do |(count, letters), output|
-      letters.each do |letter|
-        output[letter.downcase] = count
-      end
-    end
+    old.flat_map do |score, letters|
+      letters.map(&:downcase).product([score])
+    end.to_h
   end
 end
 
